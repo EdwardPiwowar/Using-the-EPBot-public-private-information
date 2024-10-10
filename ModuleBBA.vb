7a52ac95d3caadf416c7ee9485cd01f76e27b943
ed_pl <e-piwowar@wp.pl>
2024-10-10 11:09:24 +0200

    Dodaj pliki projektów.

---
+++ ModuleBBA.vb
@@ -0,0 +1,94 @@
+﻿Module ModuleBBA
+    Public Const C_PASS As Integer = 0
+    Public Const C_CLUBS As Integer = 0
+    Public Const C_DIAMONDS As Integer = 1
+    Public Const C_HEARTS As Integer = 2
+    Public Const C_SPADES As Integer = 3
+    Public Const C_NT As Integer = 4
+    Public Const C_NORTH As Integer = 0
+    Public Const C_EAST As Integer = 1
+    Public Const C_SOUTH As Integer = 2
+    Public Const C_WEST As Integer = 3
+    Public Const C_NONE As Integer = 0
+    Public Const C_NS As Integer = 2
+    Public Const C_WE As Integer = 1
+    Public Const C_BOTH As Integer = 3
+    Public Const C_FIVE As Integer = 5
+    Public Const C_LONGER As String = "AKQJT98765432"
+    Public Const C_INTERPRETED As Long = 13
+    Public Const F_MIN_HCP As Integer = 102
+    Public Const F_MAX_HCP As Integer = 103
+    Public Const F_MIN_PKT As Integer = 104
+    Public Const F_MAX_PKT As Integer = 105
+    Public board(0 To 3, 0 To 3) As Integer
+    Public dealers(0 To 15) As Integer
+    Public vulnerability(15) As Integer
+    Public strain_mark(5) As String
+    Public Structure TYPE_HAND
+        Dim suit() As String
+    End Structure
+
+    Public Sub set_board()
+        '---standard number of a board
+        board(C_NORTH, C_NONE) = 1
+        board(C_EAST, C_NS) = 2
+        board(C_SOUTH, C_WE) = 3
+        board(C_WEST, C_BOTH) = 4
+        board(C_NORTH, C_NS) = 5
+        board(C_EAST, C_WE) = 6
+        board(C_SOUTH, C_BOTH) = 7
+        board(C_WEST, C_NONE) = 8
+        board(C_NORTH, C_WE) = 9
+        board(C_EAST, C_BOTH) = 10
+        board(C_SOUTH, C_NONE) = 11
+        board(C_WEST, C_NS) = 12
+        board(C_NORTH, C_BOTH) = 13
+        board(C_EAST, C_NONE) = 14
+        board(C_SOUTH, C_NS) = 15
+        board(C_WEST, C_WE) = 16
+    End Sub
+    Public Sub set_dealers()
+        Dim k As Integer
+        For k = 0 To 12 Step 4
+            dealers(0 + k) = C_NORTH
+            dealers(1 + k) = C_EAST
+            dealers(2 + k) = C_SOUTH
+            dealers(3 + k) = C_WEST
+        Next k
+    End Sub
+    Public Sub set_vulnerability()
+        vulnerability(0) = C_NONE
+        vulnerability(1) = C_NS
+        vulnerability(2) = C_WE
+        vulnerability(3) = C_BOTH
+        vulnerability(4) = C_NS
+        vulnerability(5) = C_WE
+        vulnerability(6) = C_BOTH
+        vulnerability(7) = C_NONE
+        vulnerability(8) = C_WE
+        vulnerability(9) = C_BOTH
+        vulnerability(10) = C_NONE
+        vulnerability(11) = C_NS
+        vulnerability(12) = C_BOTH
+        vulnerability(13) = C_NONE
+        vulnerability(14) = C_NS
+        vulnerability(15) = C_WE
+    End Sub
+    Public Sub set_strain_mark()
+        strain_mark(C_CLUBS) = "C"
+        strain_mark(C_DIAMONDS) = "D"
+        strain_mark(C_HEARTS) = "H"
+        strain_mark(C_SPADES) = "S"
+        strain_mark(C_NT) = "N"
+        strain_mark(5) = ""
+    End Sub
+
+
+
+
+
+
+
+
+
+End Module
---
+++ Program.vb
@@ -0,0 +1,137 @@
+Imports System
+''EPBot is a namespace 
+Module Program
+
+
+    Sub Main(args As String())
+        Dim k As Integer, position As Integer, dealer As Integer, vulnerability As Integer, new_bid As Integer
+        Dim str_hand As String
+        Dim blocks() As String
+        Dim subblocks() As String
+        Dim suits()() As String
+        Dim hand() As TYPE_HAND
+        Dim Player(3) As EPBot86.EPBot
+        ''Dim Player(3) As EPBot64.EPBot
+        ''Dim Player(3) As EPBotARM64.EPBot
+        Dim arr_bids() As String
+        Dim arr_suits() As String
+        ReDim hand(3)
+        ReDim suits(3)
+        For k = 0 To 3
+            ReDim hand(k).suit(3)
+        Next
+
+
+
+        '---example hand
+        str_hand = "Q.K6.AQT6.KQT852 T642.Q982.842.73 AK85.T5.J975.AJ6 J973.AJ743.K3.94"
+        Console.WriteLine("Example hand: " & str_hand)
+
+        blocks = Split(str_hand, " ")
+        For k = 0 To 3
+            subblocks = Split(blocks(k), ".")
+            For i = 0 To 3
+                hand(k).suit(i) = subblocks(3 - i)
+            Next i
+        Next k
+
+        Console.WriteLine("")
+        Console.WriteLine("Entered hands:")
+        Console.WriteLine("")
+
+        For i = C_SPADES To C_CLUBS Step -1
+            Console.WriteLine(vbTab + hand(0).suit(i))
+        Next i
+        Console.WriteLine("")
+        For i = C_SPADES To C_CLUBS Step -1
+            Console.WriteLine(hand(3).suit(i) + vbTab + vbTab + hand(1).suit(i))
+        Next i
+        Console.WriteLine("")
+        For i = C_SPADES To C_CLUBS Step -1
+            Console.WriteLine(vbTab + hand(2).suit(i))
+        Next i
+        Console.WriteLine("")
+
+
+        set_board()
+        set_dealers()
+        set_vulnerability()
+        set_strain_mark()
+
+        '---example
+        position = C_NORTH
+        dealer = C_EAST
+        vulnerability = 1
+
+
+        For k = 0 To 3
+            Player(k) = New EPBot86.EPBot
+            'Player(k) = New EPBot64.EPBot
+            'Player(k) = New EPBotARM64.EPBot
+        Next k
+
+        With Player(position)
+            .new_hand(position, hand(position).suit, dealer, vulnerability)
+            .scoring = 0
+
+
+            Console.WriteLine(vbTab & vbTab & "P" & vbTab & "1D")
+            Console.WriteLine("1H" & vbTab & "3C" & vbTab & "P" & vbTab & "3H")
+            Console.WriteLine("P" & vbTab & "4N" & vbTab & "P" & vbTab & "5H")
+            Console.WriteLine("P" & vbTab & "?")
+
+
+            ReDim arr_bids(63)
+            arr_bids(0) = "00"
+            arr_bids(1) = "06"
+            arr_bids(2) = "07"
+            arr_bids(3) = "12*limit raise or better in !D"
+            arr_bids(4) = "17"
+            arr_bids(5) = "21"
+            arr_bids(6) = "00"
+            arr_bids(7) = "22"
+            arr_bids(8) = "01"
+            arr_bids(9) = "23"
+            arr_bids(10) = "00"
+            arr_bids(11) = "26"
+            arr_bids(12) = "00"
+            arr_bids(13) = "00"
+            arr_bids(14) = "00"
+
+
+
+            ''getting all hands after bidding and during play
+            For k = 0 To 3
+                Console.WriteLine("")
+                Console.WriteLine("Player cards visible from position: " & CStr(k))
+                'set hand
+                .new_hand(k, hand(k).suit, dealer, vulnerability)
+                ''set the entire auction
+                .set_arr_bids(arr_bids)
+                ''optional set dummy
+                .set_dummy(2, hand(2).suit)
+                ''optional set leads
+                '.set_arr_leads
+                ''
+                new_bid = .get_bid
+                ''
+                arr_suits = .get_arr_suits()
+
+                Console.WriteLine("")
+                For i = C_SPADES To C_CLUBS Step -1
+                    Console.WriteLine(vbTab + arr_suits(i))
+                Next i
+                Console.WriteLine("")
+                For i = C_SPADES To C_CLUBS Step -1
+                    Console.WriteLine(arr_suits(12 + i) + vbTab + vbTab + arr_suits(4 + i))
+                Next i
+                Console.WriteLine("")
+                For i = C_SPADES To C_CLUBS Step -1
+                    Console.WriteLine(vbTab + arr_suits(8 + i))
+                Next i
+                Console.WriteLine("")
+            Next k
+            Console.ReadKey()
+        End With
+    End Sub
+End Module
