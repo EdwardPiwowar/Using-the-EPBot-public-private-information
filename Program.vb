7836bc54c7319a501b28f462aba959c4633efa2b
ed_pl <e-piwowar@wp.pl>
2025-01-31 17:30:19 +0100

    Update

--- Program.vb
+++ Program.vb
@@ -3,13 +3,14 @@ Imports System.Runtime.CompilerServices
 
 Module Program
     Sub Main(args As String())
-        Dim j As Integer, k As Integer, n As Integer, new_bid As Integer
-        Dim str_hand As String, entered_hand As String
+        Dim i As Long, j As Integer, k As Integer, n As Integer
+        Dim str_hand As String
         Dim suits()() As String
         Dim arr_leads() As String
         Dim arr_leaders() As Integer
-        Dim arr_suits() As String
-        'Dim info() As Integer
+        Dim info() As Integer
+        Dim info_min() As Integer
+        Dim info_max() As Integer
         ReDim hand(3)
         ReDim suits(3)
         ReDim arr_leads(63)
@@ -29,60 +30,53 @@ Module Program
         set_vulnerability()
         set_strain_mark()
 
-        '---example hand
-        str_hand = "53.532.K87.AT872 J82.T84.AQT54.KQ AKQT94.AKQJ9.J9. 76.76.632.J96543"
-        Console.WriteLine("Current deal: " & str_hand)
-        Do
-            Console.WriteLine("Enter another deal or skip")
-            entered_hand = Console.ReadLine()
-        Loop While Len(entered_hand) <> 0 And Len(entered_hand) <> 67
-        If entered_hand = vbNullString Then
-            entered_hand = str_hand
-        End If
-        Console.WriteLine("Entered deal: " & entered_hand)
-        Console.WriteLine("")
-        set_hand(entered_hand)
-        vulnerable = 0
+
+
+        vulnerable = 3
         deal = 1
+        dealer = 0
+
+        ReDim arr_bids(63)
+        arr_bids(0) = "00"
+        arr_bids(1) = "08"
+        arr_bids(2) = "00"
+        arr_bids(3) = "14*Jacoby 2NT"
+        arr_bids(4) = "00"
+        arr_bids(5) = "16*shortness  !D"
+        arr_bids(6) = "01"
+        arr_bids(7) = "20*Cue bid, a !C stopper"
+        arr_bids(8) = "00"
+        arr_bids(9) = "24*Blackwood 0314, for !S"
+        arr_bids(10) = "00"
+        arr_bids(11) = "25*A=0/5 or 3/5"
+        arr_bids(12) = "00"
+        arr_bids(13) = "33"
+        arr_bids(14) = "00"
+        arr_bids(15) = "00"
+        arr_bids(16) = "00"
 
         For k = 0 To 3
-            dealer = (k + 0) Mod 4
-            For n = 0 To 3
-                'IMPORTANT - it is required to establish a system for both lines
-                Player(n).system_type(0) = T_21GF
-                Player(n).system_type(1) = T_21GF
-                Player(n).new_hand(n, hand((n - k + 4) Mod 4).suit, dealer, vulnerable)
-            Next n
-            Console.WriteLine("Player cards visible from position: " & CStr(k) & " - dealer = " & CStr(dealer))
-            bid()
-            'Console.WriteLine(Join(arr_bids, " "))
-            Console.WriteLine("W" + vbTab + "N" + vbTab + "E" + vbTab + "S" + vbTab)
-            Console.WriteLine(bidding_body)
-
             With Player(k)
                 'IMPORTANT - it is required to establish a system for both lines
                 .system_type(0) = T_21GF
                 .system_type(1) = T_21GF
                 'set hand
-                .new_hand(k, hand(0).suit, dealer, vulnerable)
+                '.new_hand(k, hand(k).suit, dealer, vulnerable)
                 ''set the entire auction
                 .set_arr_bids(arr_bids)
+                Console.WriteLine("Player " & k)
+                For n = 0 To 3
+                    Console.WriteLine("Position " & n)
+                    info = .info_feature(n)
+                    Console.WriteLine("HCP " & info(402) & "-" & info(403))
+                    info_min = .info_min_length(n)
+                    info_max = .info_max_length(n)
+                    For i = 0 To 3
+                        Console.WriteLine("Length " & info_min(i) & "-" & info_max(i))
+                    Next i
+                Next n
 
-                new_bid = .get_bid
-                ''obtain all hands
-                arr_suits = .get_arr_suits()
-                Console.WriteLine("")
-                For i = C_SPADES To C_CLUBS Step -1
-                    Console.WriteLine(vbTab + arr_suits(i))
-                Next i
-                Console.WriteLine("")
-                For i = C_SPADES To C_CLUBS Step -1
-                    Console.WriteLine(arr_suits(12 + i) + vbTab + vbTab + arr_suits(4 + i))
-                Next i
-                Console.WriteLine("")
-                For i = C_SPADES To C_CLUBS Step -1
-                    Console.WriteLine(vbTab + arr_suits(8 + i))
-                Next i
+
                 Console.WriteLine("")
             End With
             Console.WriteLine("")
